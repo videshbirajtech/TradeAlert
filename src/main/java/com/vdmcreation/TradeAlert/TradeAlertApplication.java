155c427c3e168920ee482bf6bf0e54033029b665
Aakash <aakash@birajtech.com>
2026-04-22 16:57:18 +0530

    first commit

---
+++ src/main/java/com/vdmcreation/TradeAlert/TradeAlertApplication.java
@@ -0,0 +1,13 @@
+package com.vdmcreation.TradeAlert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeAlertApplication.class, args);
+	}
+
+}
